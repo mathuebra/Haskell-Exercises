7ed9f2d6124d5757087a6c5dba3478a34484f1c7
mathuebra <113486353+mathuebra@users.noreply.github.com>
2023-07-17 01:40:00 -0300

    Delete binario.hs

--- binario.hs
+++
@@ -1,8 +0,0 @@
-binario :: Int -> [Int]
-
-binario n
-    | n == 0 = [0]
-    | otherwise = binarioFinal n []
-    where 
-        binarioFinal 0 lista = lista
-        binarioFinal m lista = binarioFinal (m `div` 2) ((m `mod` 2):lista)
